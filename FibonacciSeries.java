dcf9fe6eeeec7a1ba56ac585b2d1b720ed5fc6d8
VSDEV <vivek.s.sheth@gmail.com>
2015-10-07 05:58:32 -0500

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -8,28 +8,21 @@ public class FibonacciSeries {
 	{
 		//input to print Fibonacci series upto how many numbers 
 		System.out.println("Enter number upto which Fibonacci series to print: "); 
-		
 		scanner = new Scanner(System.in);
 		int number = scanner.nextInt(); 
-		
 		System.out.println("Fibonacci series upto " + number +" numbers : "); 
 		
 		//printing Fibonacci series upto number 
-		
 		for(int i=1; i<=number; i++)
 		{ 
 			System.out.print(fibo(i) +" "); 
 		}
-
-		
 	}
 	
 	private static int fibo(int n)
 	{
 		if(n <=1)
-		{
 			return n;
-		}
 		else
 			return fibo(n-1) + fibo (n-2);		
 	}
