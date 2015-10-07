002f0d3ea3a45c00407f2e0d3d924799128e4647
VSDEV <vivek.s.sheth@gmail.com>
2015-10-07 06:00:11 -0500

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,9 +5,8 @@ public class BubbleSort {
 	{
 		int a [] = {1,22,72,8,21,54,98,644,003};
 		
-		//by default flag will be asc 
+		//by default flag will be set to ascending order
 		int t [] = bubblesortFunc(a,true);
-				
 		showArray(t);
 	}
 	
@@ -16,9 +15,7 @@ public class BubbleSort {
 		for(int i=0;i<a.length;i++)
 		{
 			System.out.print(a[i]+" | ");
-			
 		}
-		
 	}
 	
 	static int[] bubblesortFunc(int []unsorted, boolean flag)
@@ -46,12 +43,7 @@ public class BubbleSort {
 					}
 				}
 			}
-			
-			//System.out.println(test[i]);
-			
 		}
 		return unsorted;
-		
 	}
-
 }
