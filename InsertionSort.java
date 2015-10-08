e8ac434a8ba2b122316252d06e45caab92769f4e
VSDEV <vivek.s.sheth@gmail.com>
2015-10-08 05:46:39 -0500

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -7,41 +7,40 @@ class ArrayInsertion
 	private long[] a; 
 	private int nElems; // number of data items
 
-public ArrayInsertion(int max) // constructor
-{
-	a = new long[max]; // create the array
-	nElems = 0; // no items yet
-}
-
-public void insert(long value) // put element into array
-{
-	a[nElems] = value; // insert it
-	nElems++; // increment size
-}
-
-public void display() // displays array contents
-{
-	for(int j=0; j<nElems; j++) // for each element,
-	System.out.print(a[j] + " "); // display it
-	System.out.println("");
-}
-
-public void insertionSort()
-{
-	int in, out;
-	for(out=1; out<nElems; out++) // out is dividing line
+	public ArrayInsertion(int max) // constructor
 	{
-		long temp = a[out]; // remove marked item
-		in = out; // start shifts at out
-		while(in>0 && a[in-1] >= temp) // until one is smaller,
+		a = new long[max]; // create the array
+		nElems = 0; // no items yet
+	}
+	
+	public void insert(long value) // put element into array
+	{
+		a[nElems] = value; // insert it
+		nElems++; // increment size
+	}
+	
+	public void display() // displays array contents
+	{
+		for(int j=0; j<nElems; j++) // for each element,
+		System.out.print(a[j] + " "); // display it
+		System.out.println("");
+	}
+	
+	public void insertionSort()
+	{
+		int in, out;
+		for(out=1; out<nElems; out++) // out is dividing line
 		{
-			a[in] = a[in-1]; // shift item to right
-			--in; // go left one position
-		}	
-		a[in] = temp; // insert marked item
-	} // end for
-} // end insertionSort()
-	//--------------------------------------------------------------
+			long temp = a[out]; // remove marked item
+			in = out; // start shifts at out
+			while(in>0 && a[in-1] >= temp) // until one is smaller,
+			{
+				a[in] = a[in-1]; // shift item to right
+				--in; // go left one position
+			}	
+			a[in] = temp; // insert marked item
+		} // end for
+	} // end insertionSort()
 } // end class ArrayInsertion
 
 
