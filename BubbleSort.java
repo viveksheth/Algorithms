f3e40b7229efb8f98176f6ba33532d108b75602d
VSDEV <vivek.s.sheth@gmail.com>
2015-10-08 05:50:21 -0500

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,31 +1,30 @@
-// bubbleSort.java
-//Author: Vivek Sheth
+// BubbleSort.java
 // to run this program: $java BubbleSort
 
 class ArrayBubble
 {
 	private long[] a; // ref to array a
 	private int nElems; // number of data items
-	//--------------------------------------------------------------
+
 	public ArrayBubble(int max) // constructor
 	{
 		a = new long[max]; // create the array
 		nElems = 0; // no items yet
 	}
-	//--------------------------------------------------------------
+
 	public void insert(long value) // put element into array
 	{
 		a[nElems] = value; // insert it
 		nElems++; // increment size
 	}
-	//--------------------------------------------------------------
+
 	public void display() // displays array contents
 	{
 		for(int j=0; j<nElems; j++) // for each element,
 		System.out.print(a[j] + " "); // display it
 		System.out.println("");
 	}
-	//--------------------------------------------------------------
+
 	public void bubbleSort()
 	{
 		int out, in;
@@ -33,17 +32,16 @@ class ArrayBubble
 		for(in=0; in<out; in++) // inner loop (forward)
 		if( a[in] > a[in+1] ) // out of order?
 		swap(in, in+1); // swap them
-} // end bubbleSort()
-//--------------------------------------------------------------
-private void swap(int one, int two)
-{
-	long temp = a[one];
-	a[one] = a[two];
-	a[two] = temp;
-}
-//--------------------------------------------------------------
-} // end class ArrayBubble
+	} // end bubbleSort()
 
+	private void swap(int one, int two)
+	{
+		long temp = a[one];
+		a[one] = a[two];
+		a[two] = temp;
+	}
+
+} // end class ArrayBubble
 
 class BubbleSort
 {
@@ -67,5 +65,4 @@ class BubbleSort
 		
 		arr.display(); // display them again
 	} // end main()
-	
 } // end class BubbleSort
