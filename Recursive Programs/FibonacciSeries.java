0bc8ab22387c1d8e84a83efc64da7a2ebe8f41d9
viveksheth <vivek.s.sheth@gmail.com>
2015-10-13 06:54:14 -0500

    FibonacciSeries
    Fibonacci Series implementation using recursion

---
+++ Recursive Programs/FibonacciSeries.java
@@ -0,0 +1,37 @@
+// FibonacciSeries.java
+// demonstrates Fibonacci Series up to the entered number using recursion
+// to run this program: $java FibonacciSeries
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+	
+	private static Scanner scanner;
+
+	public static void main(String [] arg)
+	{
+		//input to print Fibonacci series upto how many numbers 
+		System.out.println("Enter number upto which Fibonacci series to print: "); 
+		
+		scanner = new Scanner(System.in);
+		int number = scanner.nextInt(); 
+		
+		System.out.println("Fibonacci series upto " + number +" numbers : "); 
+		
+		//printing Fibonacci series upto number 		
+		for(int i=1; i<=number; i++)
+		{ 
+			System.out.print(fibo(i) +" "); 
+		}		
+	}
+	
+	private static int fibo(int n)
+	{
+		if(n <=1)
+		{
+			return n;
+		}
+		else
+			return fibo(n-1) + fibo (n-2);
+	}
+}
